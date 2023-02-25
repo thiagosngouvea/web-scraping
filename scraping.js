4766fca8fddfc360c9e866ad455d54d0d8b2fc88
thiagosngouvea <thiagosngouvea@gmail.com>
2023-02-25 19:39:44 -0300

    fix: :bug: rota de scrapt corrigida e agora traz as imagens

--- scraping.js
+++ scraping.js
@@ -1,12 +1,9 @@
 const puppeteer = require('puppeteer');
-const ExcelJS = require('exceljs');
 
 async function scrapePage() {
-
-  console.log('Iniciando o scraping');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://gregoimoveisprime.com.br/comprar-alugar/imoveis?typeArea=total_area&floorComparision=equals&sort=-created_at%2Cid&offset=1&limit=10');
+    await page.goto('https://gregoimoveisprime.com.br/comprar-alugar/imoveis?typeArea=total_area&floorComparision=equals&sort=-created_at%2Cid&offset=1&limit=1');
   
     // esperar a nova página carregar
     await page.waitForSelector('.src__Box-sc-1sbtrzs-0.sc-hlcmlc-0.jeFFeJ.CardProperty');
@@ -15,71 +12,79 @@ async function scrapePage() {
       return cards.map(card => card.querySelector('a').href);
     });
   
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Imóveis');
-  
-    worksheet.columns = [
-      { header: 'Título', key: 'title', width: 30 },
-      { header: 'Preço', key: 'price', width: 15 },
-      { header: 'Status', key: 'status', width: 15 },
-      { header: 'Detalhes', key: 'detalhes', width: 30 },
-    ];
-  
     const data = [];
   
     for (const link of propertyLinks) {
       const newPage = await browser.newPage();
       await newPage.goto(link, { waitUntil: 'networkidle2' });
+
+      await newPage.setViewport({ width: 1920, height: 1080 });
   
       await newPage.waitForSelector('.sc-1oa9ufk-1.cmAgWZ');
-
-      await newPage.click('.sc-1rjjx2i-4.dcoDlK.WrapperImage');
-
-      await newPage.waitForSelector('.sc-2l0skt-0.kUoFVh.sc-1aidao9-4.eSzcnE');
-
-      await newPage.click('.sc-2l0skt-0.kUoFVh.sc-1aidao9-4.eSzcnE');
-
+      await newPage.waitForSelector('.sc-1rjjx2i-7.daJRUc');
+      await newPage.click('.sc-1rjjx2i-7.daJRUc');
+      await newPage.waitForTimeout(3000);
+      await newPage.waitForSelector('.sc-1aidao9-3.jFtNhU');
+      await newPage.waitForTimeout(1000);
       await newPage.waitForSelector('.sc-q0jucq-2.kOoUSa');
-
       await newPage.click('.sc-q0jucq-2.kOoUSa');
-  
+      await newPage.waitForTimeout(2000);
+      await newPage.waitForSelector('.sc-q0jucq-4.gDuWtB');
+      
       const propertyData = await newPage.evaluate(async () => {
         const title = document.querySelector('.sc-de9h1g-0.cAbJFe').textContent.trim();
-        const price = document.querySelector('.sc-3hj0n0-0.kPSlSy').textContent.trim().replace(/\/\s/g, '').replace(/VENDA|ALUGUEL/g, '').replace(/\s/g, '');
+        const price = (document.querySelector('.sc-3hj0n0-0.kPSlSy') ?? document.querySelector('.sc-3hj0n0-0.bqODGa')).textContent.trim().replace(/\/\s/g, '').replace(/VENDA|ALUGUEL/g, '').replace(/\s/g, '');
         const status = document.querySelector('.sc-1lj1a6-0.fgUzYm').textContent.trim().replace(/\/\s/g, '').replace(/\s/g, '');
-  
-        const detalhesDiv = document.querySelector('.sc-1alta1m-1.ecpoTK');
-        const detalhesSpans = detalhesDiv.querySelectorAll('span');
-        const detalhes = Array.from(detalhesSpans).map(span => span.textContent.trim()).join(', ');
 
         const detailsSections = document.querySelectorAll('.sc-1gfn7xh-0.fxLMbR');
  
         const details = {};
 
         for (const section of detailsSections) {
-          const sectionTitle = section.querySelector('h3').textContent.trim();
+          let sectionTitle = section.querySelector('h3').textContent.trim();
+          sectionTitle = sectionTitle.replace(/\s+/g, '_');
+          sectionTitle = sectionTitle.replace(/[^\w\s]/gi, '');
+          sectionTitle = sectionTitle.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+          sectionTitle = sectionTitle.toLowerCase();
           const sectionSpans = section.querySelectorAll('span');
           const sectionData = Array.from(sectionSpans).map(span => span.textContent.trim()).join(', ');
           details[sectionTitle] = sectionData;
+
         }
 
-        const imagens = document.querySelectorAll('.sc-2l0skt-0.kUoFVh');
+        const fichaSection = document.querySelectorAll('.sc-vhku1u-0.hzRhgA');
 
-        const images = Array.from(imagens).map(img => img.src);
+        const ficha = {};
+
+        for (const section of fichaSection) {
+          const sectionTitle = section.querySelector('h3').textContent.trim();
+          const sectionSpans = section.querySelectorAll('span');
+          const sectionData = Array.from(sectionSpans).map(span => span.textContent.trim()).join(', ');
+          ficha[sectionTitle] = sectionData;
+        }
+
+        const imagens = document.querySelectorAll('.sc-q0jucq-5.tIXoK');
+
+        const imagensArray = [];
+
+        for (const imagem of imagens) {
+          const imagemUrl = imagem.querySelector('img').src;
+          imagensArray.push(imagemUrl);
+        }
+
+        details['Imagens'] = imagensArray;
+
 
         
-        return { title, price, status, detalhes, details, images};
+        return { title, price, status, details };
         
       });
   
-      worksheet.addRow(propertyData);
       data.push(propertyData);
       await newPage.close();
     }
-  
-    await workbook.xlsx.writeFile('imoveis.xlsx');
+    
     await browser.close();
-    console.log('Done scraping', data);
     return data;
   }
 
--- server.js
+++ server.js
@@ -19,77 +19,9 @@ app.get('/', (req, res) => {
 
 app.get('/api/scrape', async (req, res) => {
   try {
-    puppeteer.launch().then(async function (browser) {
-        const page = await browser.newPage();
-        await page.goto('https://gregoimoveisprime.com.br/comprar-alugar/imoveis?typeArea=total_area&floorComparision=equals&sort=-created_at%2Cid&offset=1&limit=10');
-      
-        // esperar a nova página carregar
-        await page.waitForSelector('.src__Box-sc-1sbtrzs-0.sc-hlcmlc-0.jeFFeJ.CardProperty');
-      
-        const propertyLinks = await page.$$eval('.src__Box-sc-1sbtrzs-0.sc-hlcmlc-0.jeFFeJ.CardProperty', cards => {
-          return cards.map(card => card.querySelector('a').href);
-        });
-      
-        const workbook = new ExcelJS.Workbook();
-        const worksheet = workbook.addWorksheet('Imóveis');
-      
-        worksheet.columns = [
-          { header: 'Título', key: 'title', width: 30 },
-          { header: 'Preço', key: 'price', width: 15 },
-          { header: 'Status', key: 'status', width: 15 },
-          { header: 'Detalhes', key: 'detalhes', width: 30 },
-        ];
-      
-        const data = [];
-      
-        for (const link of propertyLinks) {
-          const newPage = await browser.newPage();
-          await newPage.goto(link, { waitUntil: 'networkidle2' });
-      
-          await newPage.waitForSelector('.sc-1oa9ufk-1.cmAgWZ');
-      
-          const propertyData = await newPage.evaluate(async () => {
-            const title = document.querySelector('.sc-de9h1g-0.cAbJFe').textContent.trim();
-            const price = (document.querySelector('.sc-3hj0n0-0.kPSlSy') ?? document.querySelector('.sc-3hj0n0-0.bqODGa')).textContent.trim().replace(/\/\s/g, '').replace(/VENDA|ALUGUEL/g, '').replace(/\s/g, '');
-            const status = document.querySelector('.sc-1lj1a6-0.fgUzYm').textContent.trim().replace(/\/\s/g, '').replace(/\s/g, '');
-    
-            const detailsSections = document.querySelectorAll('.sc-1gfn7xh-0.fxLMbR');
-     
-            const details = {};
-    
-            for (const section of detailsSections) {
-              const sectionTitle = section.querySelector('h3').textContent.trim();
-              const sectionSpans = section.querySelectorAll('span');
-              const sectionData = Array.from(sectionSpans).map(span => span.textContent.trim()).join(', ');
-              details[sectionTitle] = sectionData;
-            }
-
-            const fichaSection = document.querySelectorAll('.sc-vhku1u-0.hzRhgA');
-
-            const ficha = {};
-
-            for (const section of fichaSection) {
-              const sectionTitle = section.querySelector('h3').textContent.trim();
-              const sectionSpans = section.querySelectorAll('span');
-              const sectionData = Array.from(sectionSpans).map(span => span.textContent.trim()).join(', ');
-              ficha[sectionTitle] = sectionData;
-            }
-
-            details['Ficha'] = ficha;
-            
-            return { title, price, status, details };
-            
-          });
-      
-          worksheet.addRow(propertyData);
-          data.push(propertyData);
-          await newPage.close();
-        }
-      
-        await workbook.xlsx.writeFile('imoveis.xlsx');
-        await browser.close();
-        res.send(data);
-      });
+    const data = await scrapePage();
+    res.send(data);
+    console.log(data)
   } catch (error) {
     console.error(error);
     res.status(500).send('Error scraping page');
